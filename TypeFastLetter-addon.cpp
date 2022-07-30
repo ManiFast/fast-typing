1fe367cf43a6bba21e36a2cb2d26c30d423f60d2
MαηiFαsт <62830326+ManiFast@users.noreply.github.com>
2022-07-30 21:51:00 +0500

    Add files via upload

---
+++ TypeFastLetter-addon.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ofstream fout;	// Write+crate
+	string path = "config.ahk";
+	fout.open(path);
+
+	string text;
+	cin >> text;
+
+	fout << "Sleep, 2000\n\n";
+
+	vector<string> textInVec(100);
+	for (int i = 0; i < text.size(); i++)
+	{
+		if (text[i] == '_')
+			textInVec[i] = ' ';
+		else
+			textInVec[i] = text[i];
+	}
+
+	for (int i = 0; i < text.size(); i++)
+	{
+		cout << textInVec[i] << " ";
+
+		if (text[i] == '_')
+			fout << "send {Space}\n";
+		else
+			fout << "send {" << textInVec[i] << "}\n";
+
+		fout << "Sleep, 1\n";
+	}
+
+
+
+
+	system("timeout 1");system("exit");fout.close();
+	return 0;
+}
